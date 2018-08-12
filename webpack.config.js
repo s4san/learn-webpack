2cb775d1bfd21a1b05a1d7351e5d095903fb6911
Sandeep Kumar <sandeepkt18@yahoo.com>
2018-08-12 11:05:21 +0530

    Add devServer configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
 const htmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  devServer: {
+    // Display only errors
+    stats: "errors-only",
+    host: process.env.HOST,
+    port: provess.env.PORT,
+    open: true,
+  },
   plugins: [
     new htmlWebpackPlugin({
-      title: 'Learn Webpack'
+      title: 'Learn Webpack',
     }),
   ],
 };
