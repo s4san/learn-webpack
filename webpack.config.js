1f891f3237b01cec8d40334533d2d4d74736d7d8
Sandeep Kumar <sandeepkt18@yahoo.com>
2018-08-12 11:37:18 +0530

    Require dotenv in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,14 @@
+const env = require('dotenv').config();
 const htmlWebpackPlugin = require('html-webpack-plugin');
+
+console.log(process.env.HOST, process.env.PORT);
 
 module.exports = {
   devServer: {
     // Display only errors
     stats: "errors-only",
     host: process.env.HOST,
-    port: provess.env.PORT,
+    port: process.env.PORT,
     open: true,
   },
   plugins: [
